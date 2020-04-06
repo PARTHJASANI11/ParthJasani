123372c73a4b6537eae24f79124a006ab9ebb185
PARTHJASANI11 <63165094+PARTHJASANI11@users.noreply.github.com>
2020-04-06 11:04:24 +0530

    Update CodeJam_1.py

--- CodeJam_1.py
+++ CodeJam_1.py
@@ -1,30 +1,22 @@
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr  4 09:03:27 2020
-
-@author: Ishika
-"""
-
 import numpy as np
 
-t = int(input())  # no of test cases
-for i in range(t):
-    count_row = 0
-    count_col = 0
+t1 = int(input())  # no of test cases
+for i in range(t1):
+    count_r = 0
+    count_c = 0
     l = []
-    size = int(input())  # size of each matrix
-    for j in range(size):
-        row = list(map(int, input().split()))
-        extra = set(row)
-        if len(row) != len(extra):
-            count_row += 1
+    s = int(input())  # size of each matrix
+    for i in range(s):
+        r = list(map(int, input().split()))
+        extra = set(r)
+        if len(r) != len(extra):
+            count_r += 1
         l.append(row)
-    arr = np.array(l)
-    trace = np.trace(arr)
-    for j in range(size):
-        col = arr[ : , j]
-        extra = set(tuple(col))
-        if len(col) != len(extra):
-            count_col += 1
-    print("Case #{}: {} {} {}".format(i + 1, trace, count_row, count_col))
+    array = np.array(l)
+    trace = np.trace(array)
+    for j in range(s):
+        c = array[ : , j]
+        extra = set(tuple(c))
+        if len(c) != len(extra):
+            count_c += 1
+    print("Case #{}: {} {} {}".format(i + 1, trace, count_r, count_c))
